8cc73dccd12a7e359bdb2517c519343a3ebb1a4b
Manish <manishmohapatra33@gmail.com>
2026-01-26 00:53:55 +0530

    pydanticoutputparser

---
+++ output_parser/pydanticoutputparser.py
@@ -0,0 +1,33 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from pydantic import BaseModel, Field
+
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="HuggingFaceH4/zephyr-7b-beta",
+    task="text-generation")
+
+model = ChatHuggingFace(llm=llm)
+
+class Person(BaseModel):
+    name: str =Field(description="The name of the person")
+    age: int = Field(gt = 18, description = "The age of the person")
+    city: str = Field(description = "The city where the person lives")
+    
+parser = PydanticOutputParser(pydantic_object = Person)
+
+template = PromptTemplate(
+    template = "Give me the name, age and city of a fictional {place} person \n {format_instruction}",
+    input_variables = ['place'],
+    partial_variables = {'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'place': 'Sri Lankan'})
+
+print(result)
